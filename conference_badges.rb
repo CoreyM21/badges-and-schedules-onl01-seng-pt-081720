81d1fd4feb80596543416892af0d76672d936a43
Corey Martin <coreyjmartin2@gmail.com>
2020-08-26 01:54:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -31,7 +31,7 @@ def badge_maker(name)
   #outputs second #assign_rooms
   
   def printer(attendees)
-    attendees.each {|attendee|
+    attendees.each do |attendee|
     puts batch_badge_creator(attendees)
     puts assign_rooms(attendees)
     end  
