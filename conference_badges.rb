537a2ab1def4cb4f1c836eee78672c7c43ea956e
Corey Martin <coreyjmartin2@gmail.com>
2020-08-26 01:01:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,19 +1,6 @@
-def badge_maker(name)
-  
 #Write method called badge_maker
- 
- "Hello, my name is #{name}."
+#puts out "Hello, my name is #{name}."
 
- #puts out "Hello, my name is #{name}."
- 
- end
- 
- #write a method called Batch_badge_creator
- def batch_badge_creator(names)
-   badges = []
-   
-  #take an array of names as an argument and returns an array of badge messages
-   
-   names.each do {|name| bagdes << badge_maker(names)}
-   badges
+def badge_maker(name)
+ "Hello, my name is #{name}."
  end
